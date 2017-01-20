5d433667004b5f88608164e64fda0a5c4305b71a
Azulean <b12345blue@gmail.com>
2017-01-19 17:25:24 -0800

    Add files via upload

---
+++ RNGthinglol.js
@@ -0,0 +1,6 @@
+double d = Math.random();
+if (d < 0.8)
+document.getElementById(
+    // 50% chance of being here
+else
+    // 30% chance of being here
---
+++ otherpage.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+<link rel="stylesheet" href="main.css" type="text/css">
+    <style type="text/css">
+	<link rel="stylesheet" href="main.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+
+    </style>
+	</head>
+	<head>
+	<style>
+
+	</style>
+	
+	<link rel="stylesheet" href="main.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Vast+Shadow" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+	</head>
+
+
+
+
+
+<body>
+<button type="button" style="background:transparent; border:none; color:transparent;" onclick="myFunction()">`</button>
+<div style="position:fixed right:100%;">
+<button type="button" style="background:transparent; border:none; color:transparent;" onclick="myFunction2()">`</button>
+</div>
+<script>
+function myFunction2() {
+    document.body.style.backgroundColor = "#rgb(14,3,16)";
+    document.body.style.backgroundImage = "url('doge2.jpg')";
+	}
+function myFunction() {
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('ianblinked.png')";
+	
+}
+</script>
+
+<div align="center">
+<div class="linky">
+<a href="http://therobodox.org" target:"_blank">Robodox</a>
+</div>
+
+<div align="center" style="position:fixed; top:10%; left:40%;">
+<div class="font-effect-3d">
+<h1>ScouDr.</h1>
+</div>
+      <div class="linky">
+        <a href="/">Enter</a>
+        </div>
+        <div class="linky">
+        <a href="/">Stats</a>
+        </div>
+        </div>
+<div style="center" style="position:fixed;">
+
+</div>
+</body>
+</html>
+
+
